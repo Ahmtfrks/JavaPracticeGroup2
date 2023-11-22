ccecaa9f6741ce72fd3ee7a8869829301faf7e79
zikkJ <zikkajovanovic@gmail.com>
2023-11-22 23:54:48 +0100

    Hi, from Zika

--- src/test/java/week1/ZikkJ.java
+++ src/test/java/week1/ZikkJ.java
@@ -3,7 +3,7 @@ package week1;
 public class ZikkJ {
 
     public static void main(String[] args) {
-        System.out.println("Hello Group 2");
+        System.out.println("Hello Group 2 -from Zika");
     }
 
 }
