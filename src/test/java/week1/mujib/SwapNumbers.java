d3a1622440601efdfe237dd9609702180cf5e3a7
mujib778 <mujibrahmansultani842@gmail.com>
2023-12-02 02:48:38 +0100

    first week task is done

---
+++ src/test/java/week1/mujib/SwapNumbers.java
@@ -0,0 +1,26 @@
+package week1.mujib;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+
+        int x = 74;
+        int y = 25;
+
+        // X = X + Y
+        x = x + y  ;
+
+        // Y = X - Y
+
+        y = x - y ;
+
+        // X = X - Y
+
+        x = x - y ;
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
+
+    }
+
+}
